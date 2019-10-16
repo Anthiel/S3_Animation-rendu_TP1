dc101ccc196985389548687544685021a4c9d35e
Antoine <Antoine.thiel83@gmail.com>
2019-10-16 15:37:17 +0200

    smoke avant le display

--- smoke.cpp
+++ smoke.cpp
@@ -1,6 +1,42 @@
 #include "smoke.h"
+#include <QVector3D>
+#include "puff.h"
 
-Smoke::Smoke()
+Smoke::Smoke(QVector3D vec, float timeInterval)
 {
+    this->timeInterval = timeInterval;
+    this->pos = vec;
+    this->elapsedTime = 0.0f;
+}
+
+void Smoke::animate(float dt){
+    elapsedTime += dt;
+
+    if(elapsedTime>timeInterval){
+        elapsedTime = 0.0f;
+        int tempsRandom = 5 + (rand() % static_cast<int>(8 - 5 + 1));
+        int sizeRandom = 1 + (rand() % static_cast<int>(5 - 1 + 1));
+        QVector3D direction(0.0, 1.0, 0.0);
+        Puff newPuff(pos, direction, static_cast<float>(sizeRandom), static_cast<float>(tempsRandom));
+        puffsList.push_back(newPuff);
+    }
+
+    std::list<Puff>::iterator i = puffsList.begin();
+    while( i != puffsList.end() )
+    {
+        if( i->lifeTimeLeft <= 0 )
+        i = puffsList.erase(i);
+        else
+        i++;
+    }
+
+    std::list<Puff>::iterator j = puffsList.begin();
+    while( j != puffsList.end() )
+    {
+        j->animate(dt);
+    }
+}
+
+void Smoke::display(){
 
 }
--- smoke.h
+++ smoke.h
@@ -1,11 +1,23 @@
 #ifndef SMOKE_H
 #define SMOKE_H
 
+#include "puff.h"
+#include <QVector3D>
+
 
 class Smoke
 {
 public:
-    Smoke();
+    Smoke(QVector3D vec, float timeInterval);
+    void animate(float dt);
+    void display();
+
+
+    QVector3D pos;
+    std::list<Puff> puffsList;
+    float timeInterval;
+    float elapsedTime;
+
 };
 
 #endif // SMOKE_H
