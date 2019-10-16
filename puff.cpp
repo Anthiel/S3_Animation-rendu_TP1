03962d8b8493e46181d9a366777ad571cca4dcda
Ptilulu83 <lucas.moragues@wanadoo.fr>
2019-10-16 14:52:58 +0200

    puff

--- puff.cpp
+++ puff.cpp
@@ -2,6 +2,29 @@
 
 Puff::Puff()
 {
-
+    this->position={0,0,0};
+    this->direction={0,0,0};
+    this->size=1;
+    this->lifeTimeLeft=0;
 }
 
+
+Puff::Puff(QVector3D position, QVector3D direction, float size, float lifeTimeLeft)
+{
+    this->position=position;
+    this->direction=direction;
+    this->size=size;
+    this->lifeTimeLeft=lifeTimeLeft;
+}
+
+Puff::animate(float dt)
+{
+    this->lifeTimeLeft-=dt;
+    if(this->lifeTimeLeft>0){
+        this->position+=dt*direction;
+    }
+}
+
+void Puff::display(){
+
+}
--- puff.h
+++ puff.h
@@ -1,11 +1,25 @@
 #ifndef PUFF_H
 #define PUFF_H
 
+#include <QVector3D>
+
 
 class Puff
 {
 public:
     Puff();
+    Puff(QVector3D position, QVector3D direction, float size, float lifeTimeLeft);
+
+    void animate(float dt);
+    void display();
+
+
+
+
+
+
+    QVector3D position,direction;
+    float size,lifeTimeLeft;
 };
 
 #endif // PUFF_H
